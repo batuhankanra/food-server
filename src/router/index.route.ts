f0487eaa3d2402367d4a12acecde9c5df0ae8031
batuhan kanra <batuhan_kanra_60@hotmail.com>
2025-05-22 00:17:50 +0300

    updates

--- src/router/index.route.ts
+++ src/router/index.route.ts
@@ -3,6 +3,9 @@ import foodRouter from './food.route'
 
 const router=Router()
 
-router.use('/food',foodRouter)
+router.use('/food',foodRouter),
+router.get('/',(req,res)=>{
+    res.json({msg:'sa'})
+})
 
 export default router
